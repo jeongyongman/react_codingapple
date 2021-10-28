cd27f3d7e7288618ff1793ab354ac041c38a6835
jeongyongman <i1683u@gmail.com>
2021-10-28 12:20:50 +0900

    03 JSX

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg'
 
 function App() {
+  // let posts = "강남 고기 맛집";
+  let posts = {color: 'blue', fontSize:'30px'};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={ posts }>개발 Blog</div>
+      </div>
+      {/* <h4>{ posts }</h4> */}
     </div>
   );
 }
